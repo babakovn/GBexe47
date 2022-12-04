3560647abc9a86e5a40f111898a564361d3e1fea
babakovn <babakovn@gmail.com>
2022-12-04 08:09:35 +0300

    Added GetRandomArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double[,] GetRandomArray(int column, int rows)
+{
+    double[,] arr = new double[column, rows];
+    Random a = new Random();
+    for(int i = 0; i < column; i++)
+        {
+            for(int j = 0; j < rows; j++)
+            {
+                arr[i,j] = a.Next();
+                Console.Write($"{arr[i,j]} ");
+            }
+        Console.WriteLine();
+        }
+    return arr;
+}
+
+//
+
+double[,] arr = GetRandomArray(column:4,
+                                rows: 3);
+
